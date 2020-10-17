83de798d5b0a0e13322fc56435d7eab7b98896f4
Yennin Muñoz <yarethgracia@gmail.com>
2020-10-16 19:51:16 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace P1_TAP_YYMG2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclose = new System.Windows.Forms.Button();
+            this.btnpink = new System.Windows.Forms.Button();
+            this.btnpurple = new System.Windows.Forms.Button();
+            this.btnyellow = new System.Windows.Forms.Button();
+            this.btngreen = new System.Windows.Forms.Button();
+            this.btnazul = new System.Windows.Forms.Button();
+            this.btnred = new System.Windows.Forms.Button();
+            this.lblx = new System.Windows.Forms.Label();
+            this.btnsaludo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnclose.Font = new System.Drawing.Font("Arial", 14F);
+            this.btnclose.Location = new System.Drawing.Point(320, 193);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(120, 35);
+            this.btnclose.TabIndex = 23;
+            this.btnclose.Text = "SALIR";
+            this.btnclose.UseVisualStyleBackColor = true;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // btnpink
+            // 
+            this.btnpink.BackColor = System.Drawing.Color.Pink;
+            this.btnpink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnpink.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpink.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.btnpink.Location = new System.Drawing.Point(450, 136);
+            this.btnpink.Name = "btnpink";
+            this.btnpink.Size = new System.Drawing.Size(120, 35);
+            this.btnpink.TabIndex = 22;
+            this.btnpink.Text = "ROSA";
+            this.btnpink.UseVisualStyleBackColor = false;
+            this.btnpink.Click += new System.EventHandler(this.btnpink_Click);
+            // 
+            // btnpurple
+            // 
+            this.btnpurple.BackColor = System.Drawing.Color.Purple;
+            this.btnpurple.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnpurple.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpurple.ForeColor = System.Drawing.Color.Violet;
+            this.btnpurple.Location = new System.Drawing.Point(450, 80);
+            this.btnpurple.Name = "btnpurple";
+            this.btnpurple.Size = new System.Drawing.Size(120, 35);
+            this.btnpurple.TabIndex = 21;
+            this.btnpurple.Text = "MORADO";
+            this.btnpurple.UseVisualStyleBackColor = false;
+            this.btnpurple.Click += new System.EventHandler(this.btnpurple_Click);
+            // 
+            // btnyellow
+            // 
+            this.btnyellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnyellow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnyellow.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnyellow.ForeColor = System.Drawing.Color.Goldenrod;
+            this.btnyellow.Location = new System.Drawing.Point(450, 29);
+            this.btnyellow.Name = "btnyellow";
+            this.btnyellow.Size = new System.Drawing.Size(120, 35);
+            this.btnyellow.TabIndex = 20;
+            this.btnyellow.Text = "AMARILLO";
+            this.btnyellow.UseVisualStyleBackColor = false;
+            this.btnyellow.Click += new System.EventHandler(this.btnyellow_Click);
+            // 
+            // btngreen
+            // 
+            this.btngreen.BackColor = System.Drawing.Color.Green;
+            this.btngreen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btngreen.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngreen.ForeColor = System.Drawing.Color.Lime;
+            this.btngreen.Location = new System.Drawing.Point(39, 136);
+            this.btngreen.Name = "btngreen";
+            this.btngreen.Size = new System.Drawing.Size(120, 35);
+            this.btngreen.TabIndex = 19;
+            this.btngreen.Text = "VERDE";
+            this.btngreen.UseVisualStyleBackColor = false;
+            this.btngreen.Click += new System.EventHandler(this.btngreen_Click);
+            // 
+            // btnazul
+            // 
+            this.btnazul.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnazul.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnazul.Font = new System.Drawing.Font("Arial", 14F);
+            this.btnazul.ForeColor = System.Drawing.Color.Teal;
+            this.btnazul.Location = new System.Drawing.Point(39, 80);
+            this.btnazul.Name = "btnazul";
+            this.btnazul.Size = new System.Drawing.Size(120, 35);
+            this.btnazul.TabIndex = 18;
+            this.btnazul.Text = "AZUL";
+            this.btnazul.UseVisualStyleBackColor = false;
+            this.btnazul.Click += new System.EventHandler(this.btnazul_Click);
+            // 
+            // btnred
+            // 
+            this.btnred.BackColor = System.Drawing.Color.Red;
+            this.btnred.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnred.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnred.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnred.Location = new System.Drawing.Point(39, 29);
+            this.btnred.Name = "btnred";
+            this.btnred.Size = new System.Drawing.Size(120, 35);
+            this.btnred.TabIndex = 17;
+            this.btnred.Text = "ROJO";
+            this.btnred.UseVisualStyleBackColor = false;
+            this.btnred.Click += new System.EventHandler(this.btnred_Click);
+            // 
+            // lblx
+            // 
+            this.lblx.AutoSize = true;
+            this.lblx.BackColor = System.Drawing.Color.Transparent;
+            this.lblx.Font = new System.Drawing.Font("Arial", 14F);
+            this.lblx.ForeColor = System.Drawing.Color.Black;
+            this.lblx.Location = new System.Drawing.Point(228, 93);
+            this.lblx.Name = "lblx";
+            this.lblx.Size = new System.Drawing.Size(0, 22);
+            this.lblx.TabIndex = 16;
+            this.lblx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblx.Click += new System.EventHandler(this.lblx_Click);
+            // 
+            // btnsaludo
+            // 
+            this.btnsaludo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnsaludo.Font = new System.Drawing.Font("Arial", 14F);
+            this.btnsaludo.Location = new System.Drawing.Point(170, 193);
+            this.btnsaludo.Name = "btnsaludo";
+            this.btnsaludo.Size = new System.Drawing.Size(120, 35);
+            this.btnsaludo.TabIndex = 15;
+            this.btnsaludo.Text = "SALUDO";
+            this.btnsaludo.UseVisualStyleBackColor = true;
+            this.btnsaludo.Click += new System.EventHandler(this.btnsaludo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 256);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btnpink);
+            this.Controls.Add(this.btnpurple);
+            this.Controls.Add(this.btnyellow);
+            this.Controls.Add(this.btngreen);
+            this.Controls.Add(this.btnazul);
+            this.Controls.Add(this.btnred);
+            this.Controls.Add(this.lblx);
+            this.Controls.Add(this.btnsaludo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnclose;
+        internal System.Windows.Forms.Button btnpink;
+        internal System.Windows.Forms.Button btnpurple;
+        internal System.Windows.Forms.Button btnyellow;
+        internal System.Windows.Forms.Button btngreen;
+        internal System.Windows.Forms.Button btnazul;
+        internal System.Windows.Forms.Button btnred;
+        internal System.Windows.Forms.Label lblx;
+        internal System.Windows.Forms.Button btnsaludo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_TAP_YYMG2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnred_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Rojo";
+        }
+
+        private void btnpink_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Rosa";
+        }
+
+        private void btnpurple_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Morado";
+        }
+
+        private void btnyellow_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Amarillo";
+        }
+
+        private void btngreen_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Verde";
+        }
+
+        private void btnazul_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "El botón es Azul";
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblx_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsaludo_Click(object sender, EventArgs e)
+        {
+            lblx.Text = "Saludos!";
+        }
+    }
+}
